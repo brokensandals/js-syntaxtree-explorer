471e5e245ee34d5eb94465da607bb376a8964dcf
Jacob Williams <jacobaw@gmail.com>
2019-12-11 16:27:49 -0800

    More initial cleanup/refactoring

--- src/components/app/app-view.js
+++ src/components/app/app-view.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+function AppView() {
   return (
     <div className="app">
       
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppView;
